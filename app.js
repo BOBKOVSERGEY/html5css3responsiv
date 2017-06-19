254f3cb4f9a50013d5fad6ea02dea832b301f746
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-06-19 10:16:28 +0300

    flexbox align-items: center, justify-content: center

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var bs = require("browser-sync").create();
 
 //тут
-bs.watch(['media-query/example-1/*.*']).on("change", bs.reload);
+bs.watch(['adaptivnaya-razmetka-img/flexbox/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  proxy: "html5css3responsiv/media-query/example-1"
+  proxy: "html5css3responsiv/adaptivnaya-razmetka-img/flexbox"
 });
